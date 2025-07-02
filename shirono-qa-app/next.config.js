787ea67b24f4035eb65f02097ee5fe817c12dea1
Hiroki Nomura <nomhiro1204@gmail.com>
2025-07-02 20:21:57 +0900

    next.config.jsの出力設定を'export'から'standalone'に変更し、静的エクスポートを有効化

--- shirono-qa-app/next.config.js
+++ shirono-qa-app/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 静的エクスポートを有効化
+  output: 'standalone', // 静的エクスポートを有効化
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'build', // Azure Static Web Appsが期待するフォルダー名
